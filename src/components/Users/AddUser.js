7cb89b685145ea9a1a39849c445805eefe2a37ec
Hristo Dimitrov <dimitrov_hristo@mail.bg>
2022-05-09 11:43:20 +0300

    commit

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,40 +1,52 @@
-import React, { useState } from "react";
+import React, { Fragment, useState, useRef } from "react";
 
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState("");
+  const nameInputRef = useRef();
+
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
+    const enteredUsername = nameInputRef.current.value;
     if (enteredUsername.trim().length === 0) {
+      setError({
+        title: "Invalid input!",
+        message: "Please enter a valid name (non-empty value)",
+      });
       return;
     }
 
     // console.log(enteredUsername);
     props.onAddUser(enteredUsername);
-    setEnteredUsername("");
+    nameInputRef.current.value = "";
   };
 
-  const usernameChageHandler = (event) => {
-    setEnteredUsername(event.target.value);
+  const errorHandler = () => {
+    setError(null);
   };
 
   return (
-    <Card className={classes.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          onChange={usernameChageHandler}
-          value={enteredUsername}
+    <Fragment>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
         />
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+      )}
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input id="username" type="text" ref={nameInputRef} />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </Fragment>
   );
 };
 
